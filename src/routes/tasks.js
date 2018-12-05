b317cebdf4b5a33ecabd6476f84660efea69c2b5
JAMS28051994 <jmadrigal_94@outlook.com>
2018-12-04 23:39:51 -0600

    Registro y listado de task sin filtro por usuario

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const task = require('../models/Task');
+const Task = require('../models/Task');
 
-router.get('Tasks/add', (req, res) => {
-    res.render('Tasks/tasks');
+router.get('/tasks/add', (req, res) => {
+    res.render('tasks/newNote');
 });
 
-router.get('/Tasks', (req, res) => {
-    res.send('Notas del usuario');
+router.post('/tasks/new-task', async (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+
+    if (!title) {
+        errors.push({ text: 'El titulo es requerido' })
+    }
+
+    if (!description) {
+        errors.push({ text: 'La descripcion es requerida' });
+    }
+
+    if (errors.length > 0) {
+        res.render('tasks/newNote', {
+            errors, title, description
+        });
+    } else {
+        const task = new Task({ title, description });
+        await task.save();
+        res.redirect('/userNotes')
+    }
+});
+
+router.get('/userNotes', async (req, res) => {
+    const userNotes = await Task.find();
+    res.render('tasks/userNotes', { userNotes });
 });
 
 module.exports = router;
