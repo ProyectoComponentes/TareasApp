aa31b839c083ecc53399ff9594a0c25293b12534
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2018-12-05 03:01:18 -0600

    estilos de la navegacion

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {isAuthenticated} = require('../helpers/autentication');
 
-router.get('/task', isAuthenticated, (request, respond) => {
+router.get('/task',isAuthenticated, (request, respond) => {
+   
     respond.render('Tasks/tasks');
 });
 module.exports = router;
